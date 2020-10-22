489eb366a040383b4d3b43fbbf91648146a9aff8
Ronit Nath <69232394+RonitNath@users.noreply.github.com>
2020-10-21 17:07:32 -0700

    Update footer.vue

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -24,11 +24,15 @@
           </g>
         </svg>
       </a>
-      <p>     We are a student group acting independently of the University of California. We take full responsibility for our organization and this web site.     </p>
+      <div>
+      <p>We are a student group acting independently of the University of California. We take full responsibility for our organization and this web site.</p>
+      </div>
+      <div>
       <a href="https://www.ocf.berkeley.edu">
     <img src="https://www.ocf.berkeley.edu/hosting-logos/ocf-hosted-penguin.svg"
         alt="Hosted by the OCF" style="border: 0;" />
 </a>
+</div>
     </div>
   </footer>
 </template>
